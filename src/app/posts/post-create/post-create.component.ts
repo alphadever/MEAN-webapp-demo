215fe2321253270d734d28a2f4c777708f071976
alpha-dever <alphadever@gmail.com>
2018-07-09 21:11:00 -0400

    added img validati

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
   private mode = 'create';
   private postId: string;
   form: FormGroup;
+  imagePreview: string;
   isLoading = false;
   post: Post;
 
@@ -23,28 +24,38 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
 
     this.form = new FormGroup({
-      'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-      'content': new FormControl(null, {validators: [Validators.required, Validators.maxLength(100)]}),
+      'title': new FormControl(null, { validators: [Validators.required, Validators.minLength(3)] }),
+      'content': new FormControl(null, { validators: [Validators.required, Validators.maxLength(100)] }),
+      'image': new FormControl(null, { validators: [Validators.required] }),
     });
 
     this.route.paramMap.subscribe((paraMap: ParamMap) => {
       if (paraMap.has('postId')) {
-        console.log('post edit');
         this.mode = 'edit';
         this.postId = paraMap.get('postId');
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          console.log(postData);
           this.post = { id: postData._id, title: postData.title, content: postData.content };
-          this.form.setValue({'title': this.post.title, 'content': this.post.content});
+          this.form.setValue({ 'title': this.post.title, 'content': this.post.content });
         });
       } else {
-        console.log('post create');
         this.mode = 'create';
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+
   }
 
   onSavePost() {
@@ -58,7 +69,6 @@ export class PostCreateComponent implements OnInit {
 
       this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
-
     this.form.reset();
   }
 }
